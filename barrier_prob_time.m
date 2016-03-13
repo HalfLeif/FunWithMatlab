aa6c537dad16f16448dd7fa5ac8067ea42f2e1e7
Leif Schelin <schelinleif@gmail.com>
2016-03-13 14:38:21 +0100

    Cleaning up in matlab code

--- barrier_prob_time.m
+++ barrier_prob_time.m
@@ -1,4 +1,4 @@
-%% Bernoulli version
+%% Methods for plotting
 
 ftitle = @(s) title(s, 'FontSize', 16);
 fxlabel = @(s) xlabel(s, 'FontSize', 14);
@@ -6,9 +6,10 @@ fylabel = @(s) ylabel(s, 'FontSize', 14);
 setlegend = @(hl) set(hl, 'FontSize', 14);
 flegend = @(c) setlegend(legend(c{:}));
 
-len = @(x) size(x, 2);
 
+%% Bernoulli version
 f = @(n, p) 1 - (1 - p).^n;
+len = @(x) size(x, 2);
 
 % p = prob of long turn
 step = 0.20;
@@ -23,7 +24,6 @@ for i = 1:num_lines
     out(i, :) = f(processes(i), p);
 end
 
-
 plot(processes, out)
 ftitle('Bernoulli model')
 fxlabel('num processes/players')
@@ -33,49 +33,54 @@ flegend(legend_cell)
 
 clear step maxn f
 
+
 %% Exp version
 
 e_cdf = @(x, lambda) 1 - exp(-lambda .* x);
 e_pdf = @(x, lambda) lambda .* exp(-lambda .* x);
 
-% Obs! Needs to support vector argument for x, not lambda!
+% Supports vector argument for x
 total_pdf = @(n, x, lambda) ...
-            e_pdf(x, lambda) .* (e_cdf(x, lambda) .^ (n - 1));
-
+            n .* e_pdf(x, lambda) .* (e_cdf(x, lambda) .^ (n - 1));
 
 for i = 1:num_lines
     n = processes(i);
     for j = 1:len(p)
         lambda = 1 ./ p(j);
-        integ = @(x) n .* x .* total_pdf(n, x, lambda);
+        integ = @(x) x .* total_pdf(n, x, lambda);
         out(i, j) = integral(integ, 0, 1000 * lambda);
     end
 end
+
 figure
 plot(processes, out)
-ftitle('Exp model, ? = 1/p')
+ftitle('Exp model, \lambda = 1/p')
 fxlabel('num processes/players')
 fylabel('estimated time')
 legend_cell = cellstr(num2str(p', 'p=%.2f'));
 flegend(legend_cell)
 
+clear lambda integ out j
+
+
 %% Plot pdf for different num players
 
-tp = 0.5;
 figure
 hold on
+
+tp = 0.5;
 players = 1:2:7;
-legend_cell = {len(players)};
+xs = 0:0.02:4;
+legend_cell = cell(1, len(players));
 for i = 1:len(players)
-    pls = players(i);
-    test_pdf = @(x) pls .* total_pdf(pls, x, 1./tp);
-    xs = 0:0.02:4;
-    plot(xs, test_pdf(xs))
-    legend_cell(i) = {[num2str(pls,'players=%i'),' ', num2str(tp,'p=%.2f')]};
+    n = players(i);
+    pdf = @(x) total_pdf(n, x, 1 ./ tp);
+    plot(xs, pdf(xs))
+    legend_cell(i) = {[num2str(n,'players=%i'),' ', num2str(tp,'p=%.2f')]};
 end
 ftitle('Pdf for different number of players/processes')
 flegend(legend_cell)
 fxlabel('time')
 fylabel('probability')
 hold off
-clear ld xs pls tp
+clear xs pls tp i n pdf legendcell
